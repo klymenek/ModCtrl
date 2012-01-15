0f4b454ba62aba96434942959cbfe11eb83af962
ares <ares@okeanos>
2012-01-15 22:47:12 +0100

    Groovy Sql

--- modctrl-webapp/src/main/java/modbus/control/server/DatabaseServiceImpl.java
+++ modctrl-webapp/src/main/java/modbus/control/server/DatabaseServiceImpl.java
@@ -1,15 +1,12 @@
 package modbus.control.server;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
+import java.sql.Connection;
+import java.util.List;
+import modbus.control.GroovySqlQuery;
 import modbus.control.client.rpc.DatabaseService;
-import modbus.control.server.db.JDBCQuery;
-import modbus.control.shared.ProcessVar;
 import modbus.control.shared.Category;
+import modbus.control.shared.ProcessVar;
 
 /**
  * 
@@ -20,22 +17,26 @@ public class DatabaseServiceImpl extends RemoteServiceServlet implements
         DatabaseService {
 
     @Override
-    public List<Category> getCategorys() {
-        try {
-            return JDBCQuery.getCategory((Connection) getServletContext().getAttribute("connection"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public List<Category> getCategorys() {        
+        return GroovySqlQuery.getCategory((Connection) getServletContext().getAttribute("connection"));
+        
+//        try {
+//            return JDBCQuery.getCategory((Connection) getServletContext().getAttribute("connection"));
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
     }
 
     @Override
     public List<ProcessVar> getVarsByCategory(Category category) {
-        try {
-            return JDBCQuery.getVars(category, (Connection) getServletContext().getAttribute("connection"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return GroovySqlQuery.getVars((Connection) getServletContext().getAttribute("connection"), category);
+        
+//        try {
+//            return JDBCQuery.getVars(category, (Connection) getServletContext().getAttribute("connection"));
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
     }
 }
